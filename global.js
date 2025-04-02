c0ed9e07dc99c8689ac83ccf99efa3e7485d6cc2
Samantha Harper <149115727+Sharper93@users.noreply.github.com>
2025-04-02 16:36:06 -0400

    Add files via upload

---
+++ global.js
@@ -0,0 +1,20 @@
+// Global Object
+
+// console.log(global);
+
+
+/* global.setTimeout(() => {
+   console.log('in the time out'); 
+   // stops int from running indefinitely -- runs once a second until setTimeout starts at 3 seconds
+   clearInterval(int);
+}, 3000);
+
+const int = setInterval(() => {
+    console.log('in the interval');
+}, 1000) ;*/
+
+// get directory structure
+console.log(__dirname);
+// get directory with current file 
+console.log(__filename);
+
